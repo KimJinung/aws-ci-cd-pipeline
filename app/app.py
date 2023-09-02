e73b7801289b781c3e13d925984e03e9f4bb7e4b
Jinung Kim <KimJinung@outlook.com>
2023-09-02 18:01:55 +0900

    Improve: modify message format

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 import sys
 
-APP_VERSION = "1.1.0"
+APP_VERSION = "1.1.1"
+
 
 def handler(event, context):
-    message = f"Hello from lambda using docker: {sys.version} '\nCurrent app version: {APP_VERSION}"
+    message = f"Hello from lambda using docker: {sys.version}\nCurrent app version: {APP_VERSION}"
     return {
         "statusCode": 200,
         "body": message
