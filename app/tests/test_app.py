7152989fc6e0d39c289d89ed2fa563ab76878aa3
Jinung Kim <KimJinung@outlook.com>
2023-09-02 18:02:16 +0900

    Improve test code

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,10 +1,10 @@
 import sys
 
-from app.app import handler
+from app.app import handler, APP_VERSION
 
 
 def test_handler():
     response = handler("", "")
 
     assert response.get("statusCode") == 200
-    assert response.get("body") == f"Hello from lambda using docker: {sys.version}"
+    assert response.get("body") == f"Hello from lambda using docker: {sys.version}\nCurrent app version: {APP_VERSION}"
